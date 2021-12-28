6a573625310c30c940c624405458851191dccbbe
Chun Yi <8.5by11@gmail.com>
2021-12-27 23:14:52 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const surprise = "surprise";
+
+function writeCards(names, surprise) {
+    let messageArray = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+
+        messageArray.push(message);
+    }
+
+    return messageArray;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+countDown(10);
+
+
+
+
+
